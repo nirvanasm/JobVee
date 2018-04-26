a2136454b2666c1422022e810606846842469c99
Sebastian Mualim <sebastianmualim@gmail.com>
2018-04-26 16:13:36 +0700

    Fix Routing and add all needed html.

--- JobVee/webapp/urls.py
+++ JobVee/webapp/urls.py
@@ -2,5 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^home', views.index, name='index'),
+    url(r'^account/login', views.login, name='login'),
+    url(r'^account/forgot', views.forgotPass, name='forgotPass'),
+    url(r'^account/userProfile', views.userProfile, name='userProfile'),
+    url(r'^searchJob', views.searchJob, name='searchJob'),
+    url(r'^searchProject', views.searchProject, name='searchProject'),
+    url(r'^editProfile', views.editProfile, name='editProfile'),
+    url(r'^account/register', views.register, name='register'),
+    url(r'^loginSearchJob', views.loginSearchJob, name='loginSearchJob'),
+    url(r'^loginSearchProject', views.loginSearchProject, name='loginSearchProject'),
+    url(r'^loginHome', views.loginIndex, name='loginHome'),
 ]
--- JobVee/webapp/views.py
+++ JobVee/webapp/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return render(request, 'webapp/index.html')
+
+def login(request):
+    return render(request, 'webapp/account/login.html')
+
+def forgotPass(request):
+    return render(request, 'webapp/account/forgot.html')
+
+def register(request):
+    return render(request, 'webapp/account/register.html')
+
+def searchJob(request):
+    return render(request, 'webapp/searchJob.html')
+
+def searchProject(request):
+    return render(request, 'webapp/searchProject.html')
+
+def editProfile(request):
+    return render(request, 'webapp/account/editProfile.html')
+
+def userProfile(request):
+    return render(request, 'webapp/account/userProfile.html')
+
+def userProfile(request):
+    return render(request, 'webapp/account/userProfile.html')
+
+def loginIndex(request):
+    return render(request, 'webapp/login/loginIndex.html')
+
+def loginSearchJob(request):
+    return render(request, 'webapp/login/loginSearchJob.html')
+
+def loginSearchProject(request):
+    return render(request, 'webapp/login/loginSearchProject.html')
+
+#This redirect function is for redirect to index.html
+def redirectHome(request):
+    return HttpResponseRedirect('webapp/index.html')
+
+def redirectLoginHome(request):
+    return HttpResponseRedirect('webapp/loginIndex.html')
+
+# Geocode Google Maps API = AIzaSyBWixlVEt6g95dhEDj3ySSwEI_z_ciO9vM
+# Distance Matrix Google API = AIzaSyCKUmrC5fneqMntGnGsEmZrzPIM33kRTIg
